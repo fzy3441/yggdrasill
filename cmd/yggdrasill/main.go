33ab9730ba19b6218b974d98741179893e9cc9fe
fzyun <fzy3441@gmail.com>
2020-09-22 13:18:28 +0800

    添加tag设置功能 添加前缀处理功能

--- cmd/yggdrasill/main.go
+++ cmd/yggdrasill/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/lpxxn/yggdrasill/code_gen"
 	"github.com/lpxxn/yggdrasill/code_gen/templates"
+	"github.com/lpxxn/yggdrasill/config"
 	"github.com/lpxxn/yggdrasill/db_meta_data"
 	"github.com/lpxxn/yggdrasill/utils"
 )
@@ -18,6 +19,9 @@ var (
 	tableNames   strFlags
 	templatePath string
 	debug        bool
+	isJsonTag    bool
+	tagName      string
+	prefix       string
 )
 
 type strFlags []string
@@ -34,17 +38,26 @@ func (i *strFlags) Set(value string) error {
 func init() {
 	flag.StringVar(&target, "target", "postgresql", "mysql postgresql[pg]")
 	flag.StringVar(&dsn, "dsn", "postgresql", "dsn")
-	flag.StringVar(&destDir, "dir", "./tmp", "Destination dir for files generated.")
+	flag.StringVar(&destDir, "dir", "./model", "Destination dir for files generated.")
 	flag.StringVar(&packageName, "package_name", "model", "package name default model.")
 	flag.StringVar(&templatePath, "template_path", "", "custom template file path")
 	flag.Var(&tableNames, "table_names", "if it is empty, will generate all tables in database")
 	flag.BoolVar(&debug, "debug", false, "debug")
+	flag.BoolVar(&isJsonTag, "is_json_tag", true, "default is true and show json tag")
+	flag.StringVar(&tagName, "tag", "db", "default is 'db' and show json tag is 'db' ")
+	flag.StringVar(&prefix, "prefix", "", "set table prefix")
+
 }
 
 func main() {
 	flag.Parse()
-	utils.CleanUpGenFiles(destDir)
-	utils.MkdirPathIfNotExist(destDir)
+	config.Global.PackageName = packageName
+	config.Global.DestDir = destDir
+	config.Global.IsJsonTag = isJsonTag
+	config.Global.TagName = tagName
+	config.Global.Prefix = prefix
+	utils.CleanUpGenFiles(config.Global.DestDir)
+	utils.MkdirPathIfNotExist(config.Global.DestDir)
 	dbMetaData, err := code_gen.NewDbCodeGen(target)
 	if err != nil {
 		fmt.Println("unsupported db type, please input mysql postgresql[pg]")
@@ -74,7 +87,7 @@ func main() {
 	}
 	for _, item := range tables {
 		body, err := code_gen.GenerateTemplate(temp.GetTemplate(), item, map[string]interface{}{
-			"packageName": packageName,
+			"packageName": config.Global.PackageName,
 		})
 		if debug {
 			fmt.Println(string(body))
@@ -83,7 +96,7 @@ func main() {
 			fmt.Printf("GenerateTemplate err: %#v", err.Error())
 			return
 		}
-		if err := utils.SaveFile(destDir, item.Name+".go", body); err != nil {
+		if err := utils.SaveFile(destDir, item.StructName()+".go", body); err != nil {
 			fmt.Printf("save file error: %#v", err)
 			return
 		}
